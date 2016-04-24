c4a551e85acd3e9a725556b6c8f7e49b42b95fe3
godrose <godrose@gmail.com>
2016-04-24 13:49:26 +0300

    Added TestsWithFake configuration - needs some changes. having Specflow project with specs and adapters that don't run tests but provide smart completion which is good enough at this point.

---
+++ src/Samples.WinRT.Client.Tests.Steps.Adapters/LoginStepsAdapter.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Samples.WinRT.Client.Tests.Steps.Adapters
+{
+    [Binding]
+    public sealed class LoginStepsAdapter
+    {
+        [When(@"I set the username to ""(.*)""")]
+        public void WhenISetTheUsernameTo(string username)
+        {
+            LoginSteps.WhenISetTheUsernameTo(username);
+        }
+
+        [When(@"I log in to the system")]
+        public void WhenILogInToTheSystem()
+        {
+            LoginSteps.WhenILogInToTheSystem();
+        }
+
+        [Then(@"the login screen is displayed")]
+        public void ThenTheLoginScreenIsDisplayed()
+        {
+            LoginSteps.ThenTheLoginScreenIsDisplayed();
+        }
+
+        [Then(@"Application navigates to the main screen")]
+        public void ThenApplicationNavigatesToTheMainScreen()
+        {
+            //for readability reasons
+        }
+    }
+}
--- src/Samples.WinRT.Client.Tests.Steps/LoginSteps.cs
+++ src/Samples.WinRT.Client.Tests.Steps/LoginSteps.cs
@@ -1,4 +1,5 @@
-﻿using Attest.Testing.Core;
+﻿using System;
+using Attest.Testing.Core;
 using Samples.WinRT.Client.ScreenObjects.Contracts;
 using Shouldly;
 
@@ -12,6 +13,16 @@ namespace Samples.WinRT.Client.Tests.Steps
         {
             var isActive = _loginScreenObject.IsActive();
             isActive.ShouldBeTrue();
+        }
+
+        public static void WhenISetTheUsernameTo(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void WhenILogInToTheSystem()
+        {
+            throw new NotImplementedException();
         }
     }
 }
